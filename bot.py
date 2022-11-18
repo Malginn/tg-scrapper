31ff2c9f6cd4be9c74f297acb76cd1f8e35851f8
Malginn <ivanov.na7069@gmail.com>
2022-11-19 00:28:26 +0400

    50/50 fix sending

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
-from url import dict_
+# from url import dict_
 from main import get_data_with_selenium
 import os
 import shutil
@@ -10,41 +10,41 @@ translator = Translator()
 API_TOKEN = '5692130473:AAFYtJiFHRfw2Rh1lLeDb1e7fxdywH3575U'
 
 # URL для тестов сделал
-URL = 'https://item.taobao.com/item.htm?id=687815414870&price=194.99&sourceType=item&sourceType=item&suid=f4ab6dfd-3c89-4606-81a7-b9b74ad5adb9&ut_sk=1.YgbGi%2FkQyU0DAD3WQG9i%2BcAZ_21646297_1668264784118.Copy.ShareGlobalNavigation_1&un=75b8c07d63d793d3b869032d15dddb3f&share_crt_v=1&un_site=0&spm=a2159r.13376460.0.0&sp_abtk=gray_ShareGlobalNavigation_1_code_simpleAndroid&tbSocialPopKey=shareItem&sp_tk=dDZDYmQxck1TdW4%3D&cpp=1&shareurl=true&short_name=h.UgFnSN4&bxsign=scd7fVjWiuAdaVF9IWPRnH-aVCofWKHVNbA9YOC3YWLrrDMwNZgLit2jZUBIBDj6aNhOlcPfcXr_odogmSCQeRh6J4dKjZVoBf6uA-NLYQRYgB-5yC2nYyX5N4fDyv9RViOgmgYDKfBaP4phh2IL6qczQ&tk=t6Cbd1rMSun&app=chrome&price=194.99&sourceType=item&sourceType=item&suid=f4ab6dfd-3c89-4606-81a7-b9b74ad5adb9&ut_sk=1.YgbGi%2FkQyU0DAD3WQG9i%2BcAZ_21646297_1668264784118.Copy.ShareGlobalNavigation_1&un=75b8c07d63d793d3b869032d15dddb3f&share_crt_v=1&un_site=0&spm=a2159r.13376460.0.0&sp_abtk=gray_ShareGlobalNavigation_1_code_simpleAndroid&tbSocialPopKey=shareItem&sp_tk=dDZDYmQxck1TdW4%3D&cpp=1&shareurl=true&short_name=h.UgFnSN4&bxsign=scd7fVjWiuAdaVF9IWPRnH-aVCofWKHVNbA9YOC3YWLrrDMwNZgLit2jZUBIBDj6aNhOlcPfcXr_odogmSCQeRh6J4dKjZVoBf6uA-NLYQRYgB-5yC2nYyX5N4fDyv9RViOgmgYDKfBaP4phh2IL6qczQ&tk=t6Cbd1rMSun&app=chrome'
+URL = 'https://m.tb.cn/h.UgFnSN4?tk=t6Cbd1rMSun'
 
 
 def prepare_item(dict_value):
     string = f'''
 Название: {dict_value['name']}\n
 Продавец: {dict_value['seller']}\n
-Цена: {price_split()}\n
+Цена: {price_split(dict_value)}\n
 Размер: {dict_value['size']}\n
 Доставка: {dict_value['delivery']}\n
 Цвет: {dict_value['color']}\n
-Характеристика: {charac()}\n
+Характеристика: {charac(dict_value)}\n
              '''    # 'Картинка': {dict_value['image']
 
     return string
 
 
-def charac():
-    str_ch = '\n'.join(dict_['characteristic']) 
+def charac(data):
+    str_ch = '\n'.join(data['characteristic']) 
     return str_ch
 
 
-def price_split():
-    str_pr = ' '.join(dict_['price'])
+def price_split(data):
+    str_pr = ' '.join(data['price'])
     return str_pr
 
 
-def translate_text():
-    for k, v in dict_.items():
+def translate_text(data):
+    for k, v in data.items():
         if type(v) == list:
-            for count, i in enumerate(dict_[k]):
-                dict_[k][count] = translator.translate(i, src='zh-tw', dest='ru').text
+            for count, i in enumerate(data[k]):
+                data[k][count] = translator.translate(i, src='zh-tw', dest='ru').text
         else:
-            dict_[k] = translator.translate(v, src='zh-tw', dest='ru').text
-        return dict_
+            data[k] = translator.translate(v, src='zh-tw', dest='ru').text
+    return data
 
 
 def clear_img():
@@ -66,14 +66,19 @@ async def start_cmd_handler(message: types.Message):
 @dp.message_handler()
 async def all_msg_handler(message: types.Message):
     text = message.text
-    data = get_data_with_selenium(URL)  # URL заменить на message.text
+    data = get_data_with_selenium(text)  # URL заменить на message.text
     media = types.MediaGroup()
     for image in data['image']:
-        if data['image'].index(image) == len(dict_['image'])-1:
-            media.attach_photo(types.InputFile(f'./images/{image}'), text)  # text заменить на результат обработки data
+        if data['image'].index(image) == len(data['image'])-1:
+            media.attach_photo(types.InputFile(f'./images/{image}'), )  # text заменить на результат обработки data   , prepare_item(data)
         else:
             media.attach_photo(types.InputFile(f'./images/{image}'))
     await message.answer_media_group(media=media)
+    print('sent photo')
+    data = translate_text(data)
+    print('text translated')
+    await message.answer(f'{prepare_item(data)}\n{text}')
+    print('text sent')
     clear_img()
 
 
--- url.py
+++ url.py
@@ -1,7 +1,18 @@
 #coding: utf-8
+from googletrans import Translator
+translator = Translator()
 
+
+def translate_text(data):
+    for k, v in data.items():
+        if type(v) == list:
+            for count, i in enumerate(data[k]):
+                data[k][count] = translator.translate(i, src='zh-tw', dest='ru').text
+        else:
+            data[k] = translator.translate(v, src='zh-tw', dest='ru').text
+    return data
 
 URL = ''
 
 dict_ = {'name': 'Yi Z CLUB 设计感后背钉珠字母宽松纯棉牛仔夹克外套秋冬女装0.83', 'seller': 'Yi Z CLUB 高端女装 原衣庄外贸', 'price': ['194.99'], 'size': ['CHN-XS', 'CHN-S'], 'delivery': '快递 ¥10.00', 'color': '黑灰[22-10-24]', 'characteristic': ['品牌: other/其他', '尺码: CHN-XS CHN-S', '面料: 其他/other', '图案: 字母/数字/文字', '风格: 街头', '领子: 其他/other', '衣门襟: 单排扣', '颜色分类: 黑灰[22-10-24]', '袖型: 常规', '组合形式: 单件', '货号: ***', '年份季节: 2022年秋季', '袖长: 长袖', '厚薄: 常规', '衣长: 常规', '服装版型: 宽松型', '流行元素/工艺: 口袋 钉珠 纽扣', '材质成分: 棉100%'], 'image': []}
-
+print(translate_text(dict_))
